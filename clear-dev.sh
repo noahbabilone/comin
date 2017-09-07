8a777d524fbfa7e473a663a2107153ad7433ceba
noahbabilone <noahbabilone@gmail.com>
2017-09-07 17:19:18 +0200

    Updated Clear-dev

--- clear-dev.sh
+++ clear-dev.sh
@@ -1,1 +1,1 @@
-php bin/console cache:clearphp bin/console cache:warmupphp bin/console assets:install --symlinkphp bin/console assetic:dumpchown -R www-data:www-data .
+#!/usr/bin/env bashphp bin/console cache:clearphp bin/console cache:warmupphp bin/console assets:install --symlinkphp bin/console assetic:dumpchown -R www-data:www-data .
