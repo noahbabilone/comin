4667a7d2752bc27ddd9fbe6bdef392d824daa4d8
noahbabilone <noahbabilone@gmail.com>
2017-09-08 18:26:16 +0200

    Remove APIBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new BackBundle\BackBundle(),
-            new APIBundle\APIBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
