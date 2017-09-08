ebad4338e9a689b8768efa8fa2077f627ddbb259
noahbabilone <noahbabilone@gmail.com>
2017-09-08 10:54:47 +0200

    Reset CSS Template

---
+++ web/public/assets/pages/scripts/login-4.js
@@ -0,0 +1,157 @@
+var Login = function () {
+
+	var handleLogin = function() {
+		$('.login-form').validate({
+	            errorElement: 'span', //default input error message container
+	            errorClass: 'help-block', // default input error message class
+	            focusInvalid: false, // do not focus the last invalid input
+	            rules: {
+	                _username: {
+	                    required: true
+	                },
+	                _password: {
+	                    required: true
+	                },
+	                remember: {
+	                    required: false
+	                }
+	            },
+
+	            messages: {
+	                _username: {
+	                    required: "Login est obligatoire."
+	                },
+	                _password: {
+	                    required: "Mot de passe est obligatoire."
+	                }
+	            },
+
+	            invalidHandler: function (event, validator) { //display error alert on form submit   
+	                $('.alert-danger', $('.login-form')).show();
+	            },
+
+	            highlight: function (element) { // hightlight error inputs
+	                $(element)
+	                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+	            },
+
+	            success: function (label) {
+	                label.closest('.form-group').removeClass('has-error');
+	                label.remove();
+	            },
+
+	            errorPlacement: function (error, element) {
+	                error.insertAfter(element.closest('.input-icon'));
+	            },
+
+	            submitHandler: function (form) {
+	                form.submit();
+	            }
+	        });
+
+	        $('.login-form input').keypress(function (e) {
+	            if (e.which == 13) {
+	                if ($('.login-form').validate().form()) {
+	                    $('.login-form').submit();
+	                }
+	                return false;
+	            }
+	        });
+        
+         jQuery('#forget-password').click(function () {
+	            jQuery('.login-form').hide();
+	            jQuery('.forget-form').show();
+	        });
+	}
+
+	var handleForgetPassword = function () {
+		$('.forget-form').validate({
+	            errorElement: 'span', //default input error message container
+	            errorClass: 'help-block', // default input error message class
+	            focusInvalid: false, // do not focus the last invalid input
+	            ignore: "",
+	            rules: {
+	                email: {
+	                    required: true,
+	                    email: true
+	                }
+	            },
+
+	            messages: {
+	                email: {
+	                    required: "Email est obligatoire."
+	                }
+	            },
+
+	            invalidHandler: function (event, validator) { //display error alert on form submit   
+
+	            },
+
+	            highlight: function (element) { // hightlight error inputs
+	                $(element)
+	                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+	            },
+
+	            success: function (label) {
+	                label.closest('.form-group').removeClass('has-error');
+	                label.remove();
+	            },
+
+	            errorPlacement: function (error, element) {
+	                error.insertAfter(element.closest('.input-icon'));
+	            },
+
+	            submitHandler: function (form) {
+	                form.submit();
+	            }
+	        });
+
+	        $('.forget-form input').keypress(function (e) {
+	            if (e.which == 13) {
+	                if ($('.forget-form').validate().form()) {
+	                    $('.forget-form').submit();
+	                }
+	                return false;
+	            }
+	        });
+
+	        jQuery('#forget-password').click(function () {
+	            jQuery('.login-form').hide();
+	            jQuery('.forget-form').show();
+	        });
+
+	        jQuery('#back-btn').click(function () {
+	            jQuery('.login-form').show();
+	            jQuery('.forget-form').hide();
+	        });
+
+	}
+
+    
+    return {
+        //main function to initiate the module
+        init: function () {
+        	
+            handleLogin();
+            handleForgetPassword();
+           // handleRegister();    
+
+            // init background slide images
+		    $.backstretch([
+		        "../bundles/back/theme/assets/pages/media/bg/1.jpg",
+		        "../bundles/back/theme/assets/pages/media/bg/2.jpg",
+		        "../bundles/back/theme/assets/pages/media/bg/3.jpg",
+		        "../bundles/back/theme/assets/pages/media/bg/4.jpg"
+		        ], {
+		          fade: 1000,
+		          duration: 8000
+		    	}
+        	);
+        }
+    };
+
+}();
+
+jQuery(document).ready(function() {
+    Login.init();
+});
